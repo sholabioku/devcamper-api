418db3564e17996fffab5785bbca852a8ba2035b
sholabioku <bilushonline@gmail.com>
2020-12-10 09:17:43 +0100

    Add .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,9 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
   },
-  rules: {
-  },
+  rules: {},
 };
