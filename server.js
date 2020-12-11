f463c232d4999fee00958f9685125e8945bd89db
sholabioku <bilushonline@gmail.com>
2020-12-11 14:53:16 +0100

    Add GET /api/v1/bootcamps route

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
