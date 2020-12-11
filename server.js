a57a996c80c7027ec36f188d5e9af9c2f208a60d
sholabioku <bilushonline@gmail.com>
2020-12-11 14:54:56 +0100

    Add GET /api/v1/bootcamps/:id route

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/api/v1/bootcamps', (req, res) => {
   res.status(200).json({ success: true, msg: 'Show all bootcamps' });
 });
 
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
