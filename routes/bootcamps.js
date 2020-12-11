3fa364d15e89e3559a4eb924be831b2182e34a00
sholabioku <bilushonline@gmail.com>
2020-12-11 15:21:30 +0100

    Import bootcamps.js file and mount it

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,27 +2,27 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/api/v1/bootcamps', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).json({ success: true, msg: 'Show all bootcamps' });
 });
 
-router.get('/api/v1/bootcamps/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
 });
 
-router.post('/api/v1/bootcamps', (req, res) => {
+router.post('/', (req, res) => {
   res.status(200).json({ success: true, msg: `Create bootcamp` });
 });
 
-router.put('/api/v1/bootcamps/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 });
 
-router.delete('/api/v1/bootcamps/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
