088c1a10d093a35891c25bf96c99a586899fc6e9
sholabioku <bilushonline@gmail.com>
2020-12-11 17:00:41 +0100

    Use morgan middleware to log in development

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 const bootcamps = require('./routes/bootcamps');
-const logger = require('./middleware/logger');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.use(logger);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
