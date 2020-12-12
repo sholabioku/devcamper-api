7f52744aed5247914d0fbe06a21f2ed73d051c4e
sholabioku <bilushonline@gmail.com>
2020-12-12 17:44:02 +0100

    Fetch single bootcamp for database functionality

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -15,10 +15,16 @@ exports.getBootcamps = async (req, res, next) => {
 // @desc Get single bootcamp
 // @route GET /api/v1/bootcamps/:id
 // @access Public
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc Create bootcamp
