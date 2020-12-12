d10bb64d1e9a6f36c0b1f8f2937bc1b41fbb5f54
sholabioku <bilushonline@gmail.com>
2020-12-12 14:09:33 +0100

    Add global unhandled promise rejection

--- server.js
+++ server.js
@@ -20,7 +20,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandle promise rejection;
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
