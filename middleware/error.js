f08096c19484e9e48550235fd5b3f160dc222f52
sholabioku <bilushonline@gmail.com>
2020-12-14 10:13:53 +0100

    Add mongoose validationError catch

--- middleware/error.js
+++ middleware/error.js
@@ -14,6 +14,11 @@ const errorHandler = (err, req, res, next) => {
     const message = 'Duplicate field value entered';
     error = new ErrorResponse(message, 400);
   }
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorResponse(message, 400);
+  }
   res
     .status(error.statusCode || 500)
     .json({ success: false, error: error.message || 'Server error' });
