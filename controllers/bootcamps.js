6e571d2ba7b7046d830d94d8b810c1faafcab324
sholabioku <bilushonline@gmail.com>
2020-12-14 08:04:29 +0100

    Update error response

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -31,12 +31,7 @@ exports.getBootcamp = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    next(
-      new ErrorResponse(
-        `Bootcamp with id of ${req.params.id} is not found`,
-        404
-      )
-    );
+    next(err);
   }
 };
 
@@ -66,7 +61,12 @@ exports.updateBootcamp = async (req, res, next) => {
     });
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(
+          `Bootcamp with id of ${req.params.id} is not found`,
+          404
+        )
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
@@ -83,7 +83,12 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(
+          `Bootcamp with id of ${req.params.id} is not found`,
+          404
+        )
+      );
     }
 
     res.status(200).json({ success: true });
