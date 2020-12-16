db0546c6a2dcce6429bfa1cc9bf1b955021fff1d
sholabioku <bilushonline@gmail.com>
2020-12-16 13:44:59 +0100

    Add select and sort query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,12 +8,41 @@ const geocoder = require('../utils/geocoder');
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  // Convert query object to string
-  let queryStr = JSON.stringify(req.query);
+  let query;
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ['select', 'sort'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators like (lt, lte, gt)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/, (match) => `$${match}`);
 
-  const query = JSON.parse(queryStr);
-  const bootcamps = await Bootcamp.find(query);
+  // Finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Executing query
+  const bootcamps = await query;
   res
     .status(200)
     .json({ success: true, count: bootcamps.length, data: bootcamps });
