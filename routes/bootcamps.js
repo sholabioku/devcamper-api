2ff69dd9dd8c66a552e8a0eb710892a0e508d081
sholabioku <bilushonline@gmail.com>
2020-12-17 17:54:49 +0100

    Add PUT /api/v1/bootcamps/:id/photo route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,7 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
 
 // Include other resource router
@@ -17,6 +18,7 @@ const courseRouter = require('./courses');
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
+router.route('/:id/photo').put(bootcampPhotoUpload);
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/').get(getBootcamps).post(createBootcamp);
 router
