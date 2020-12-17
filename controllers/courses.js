f5822218245eefdc21b2f77fd7aaa35fa8fcfb0e
sholabioku <bilushonline@gmail.com>
2020-12-17 12:03:35 +0100

    Add updateCourse method and its functionality

--- controllers/courses.js
+++ controllers/courses.js
@@ -56,3 +56,20 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, data: course });
 });
+// @desc Update course
+// @route PUT /api/v1/courses/:id
+// @access Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+  course = await Course.updateOne(req.params.id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+
+  res.status(200).json({ success: true, data: course });
+});
