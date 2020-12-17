eee4c1030a77fac8de00daa5b5eba60c887e554e
sholabioku <bilushonline@gmail.com>
2020-12-17 17:48:02 +0100

    Add bootcampPhotoUpload method, find the bootcamp and check for file

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -172,3 +172,20 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     .status(200)
     .json({ success: true, count: bootcamps.length, data: bootcamps });
 });
+
+// @desc Upload photo for bootcamp
+// @route PUT /api/v1/bootcamps/:id/photo
+// @access Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  if (!req.files) {
+    return next(new ErrorResponse('Please upload a file', 400));
+  }
+});
