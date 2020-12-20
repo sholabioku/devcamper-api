be791b64ea2e248037966782908bfef784d3e8e3
sholabioku <bilushonline@gmail.com>
2020-12-20 10:29:20 +0100

    Add protect func and its functionality

---
+++ middleware/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
+
+exports.protect = async function (req, res, next) {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  // else if (req.cookies) {
+  //   token = req.cookies.token;
+  // }
+
+  if (!token) {
+    return next(new ErrorResponse('Not authorize to access this route', 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    console.log(decoded);
+
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (err) {
+    return next(new ErrorResponse('Not authorize to access this route', 401));
+  }
+};
