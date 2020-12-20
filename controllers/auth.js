eb3f15729bacafae6e15382661e146d9551d3195
sholabioku <bilushonline@gmail.com>
2020-12-20 06:57:52 +0100

    Add login method

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,3 +13,28 @@ exports.register = asyncHandler(async (req, res, next) => {
   const token = user.generateAuthToken();
   res.status(200).json({ success: true, token });
 });
+
+// @desc Login User
+// @route POST /api/v1/auth/login
+// @access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse('Please add email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const token = user.generateAuthToken();
+  res.status(200).json({ success: true, token });
+});
