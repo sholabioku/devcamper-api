3eecfe6fcf4c16ef67fe2dc2757c33ba3a590d75
sholabioku <bilushonline@gmail.com>
2020-12-20 04:02:59 +0100

    Return auth token for the user

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,5 +10,6 @@ exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
   const user = await User.create({ name, email, password, role });
 
-  res.status(200).json({ success: true });
+  const token = user.generateAuthToken();
+  res.status(200).json({ success: true, token });
 });
