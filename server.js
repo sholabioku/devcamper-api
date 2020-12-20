09bd8597a1f6b701ef3e0b5d343e36dccaef0379
sholabioku <bilushonline@gmail.com>
2020-12-20 03:17:57 +0100

    Mount /api/v1/auth route

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ connectDB();
 
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
