0757eb97fef45db1ae6522bd22d6960c51c56b0e
sholabioku <bilushonline@gmail.com>
2020-12-20 11:39:24 +0100

    Add GET /api/v1/auth/me route

--- models/User.js
+++ models/User.js
@@ -41,7 +41,7 @@ UserSchema.pre('save', async function (next) {
 });
 
 UserSchema.methods.generateAuthToken = function () {
-  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
   });
 };
