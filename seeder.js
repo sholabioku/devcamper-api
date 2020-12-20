c36ed7ef2d20f511eccf0b6237a45a1f71ee763d
sholabioku <bilushonline@gmail.com>
2020-12-20 15:13:30 +0100

    Seed user data to database

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({ path: './config/config.env' });
 
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
@@ -24,11 +25,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
 
     console.log('Data Imported...'.green.inverse);
     process.exit();
@@ -42,6 +48,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log('Data destroyed...'.red.inverse);
     process.exit();
