b3ff33099abd15a2bbaa9cd65802c35e45d9bd79
sholabioku <bilushonline@gmail.com>
2020-12-20 12:32:02 +0100

    Add authorize method and its functionality

--- middleware/auth.js
+++ middleware/auth.js
@@ -29,3 +29,17 @@ exports.protect = async function (req, res, next) {
     return next(new ErrorResponse('Not authorize to access this route', 401));
   }
 };
+
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
