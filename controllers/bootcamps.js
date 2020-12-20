d6b0de56dac48ecc4ca7f463347c3341cc6cf4e7
sholabioku <bilushonline@gmail.com>
2020-12-20 14:59:30 +0100

    Add user to request body

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -32,6 +32,9 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route POST /api/v1/bootcamps
 // @access Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req.body
+  req.body.user = req.user.id;
+
   const bootcamp = await Bootcamp.create(req.body);
 
   res
