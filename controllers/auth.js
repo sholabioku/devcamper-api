ac1179c6beb5e59c7bd76cf939a965b2c4214a0a
sholabioku <bilushonline@gmail.com>
2020-12-21 07:42:47 +0100

    Save token to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,6 +56,10 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   // Get reset token
   const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({ success: true, data: user });
 });
 
 const sendTokenResponse = (user, statusCode, res) => {
