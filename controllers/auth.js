dc70312c90c5d1f2f5cd0408c32bbe43c9c3976e
sholabioku <bilushonline@gmail.com>
2020-12-21 07:02:11 +0100

    Add forgotPassword method

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,6 +37,27 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc Get current logged in user
+// @route GET /api/v1/auth/me
+// @access Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({ success: true, data: user });
+});
+
+// @desc Forgot Password
+// @route POST /api/v1/auth/forgotPassword
+// @access Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new ErrorResponse('There is user with that email', 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+});
+
 const sendTokenResponse = (user, statusCode, res) => {
   const token = user.generateAuthToken();
 
@@ -56,11 +77,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     .cookie('token', token, options)
     .json({ success: true, token });
 };
-
-// @desc Get current logged in user
-// @route GET /api/v1/auth/me
-// @access Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-  res.status(200).json({ success: true, data: user });
-});
