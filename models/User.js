8a65d5767394ef133318862bfdfd2c16cf57d3e6
sholabioku <bilushonline@gmail.com>
2020-12-21 07:42:12 +0100

    Check if isModified password

--- models/User.js
+++ models/User.js
@@ -37,6 +37,10 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
