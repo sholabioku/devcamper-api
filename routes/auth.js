562d7d7645f84bcf37f74bff37f6081bc732e906
sholabioku <bilushonline@gmail.com>
2020-12-23 13:53:57 +0100

    Add PUT /api/v1/auth/updatePassword route

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const {
   forgotPassword,
   resetPassword,
   updateDetails,
+  updatePassword,
 } = require('../controllers/auth');
 
 const { protect } = require('../middleware/auth');
@@ -19,5 +20,6 @@ router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 
 module.exports = router;
