53fc0a49b8ecafac9e0ee0c744b4c6d3d48c5d42
sholabioku <bilushonline@gmail.com>
2020-12-24 10:38:27 +0100

    Add GET GET /api/v1/bootcamps/:bootcampId/reviews routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -19,9 +19,11 @@ const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource router
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router
   .route('/:id/photo')
