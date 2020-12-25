ea3628d1eaff5026fbbc7edb49ec3bd1b126461c
sholabioku <bilushonline@gmail.com>
2020-12-25 15:25:37 +0100

    Add averageRating calculation functionality

--- models/Review.js
+++ models/Review.js
@@ -12,7 +12,7 @@ const ReviewSchema = new mongoose.Schema({
     required: [true, 'Please add a text'],
   },
   rating: {
-    type: String,
+    type: Number,
     min: 1,
     max: 10,
     required: [true, 'Please add a rating between 1 and 10'],
@@ -31,6 +31,39 @@ const ReviewSchema = new mongoose.Schema({
     ref: 'User',
     required: true,
   },
+});
+
+// Prevent from submitting more than one review per bootcamp
+ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
+
+// Static method to get avg for review rating
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: { _id: '$bootcamp', averageRating: { $avg: '$rating' } },
+    },
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Call getAverageRating after save
+ReviewSchema.post('save', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before remove
+ReviewSchema.pre('remove', function () {
+  this.constructor.getAverageRating(this.bootcamp);
 });
 
 module.exports = mongoose.model('Review', ReviewSchema);
