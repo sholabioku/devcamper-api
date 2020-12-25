3a6bf2d4663f83aab177b09c30ab34b17db8eca0
sholabioku <bilushonline@gmail.com>
2020-12-25 17:07:54 +0100

    Add updateReview method with its functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -59,3 +59,26 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 
   res.status(201).json({ success: true, data: review });
 });
+
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review;
+  review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure review belong to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse('Not authorized to update review', 401));
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: review });
+});
