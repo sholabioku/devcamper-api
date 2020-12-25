4012fe92714f07c465a59c1ed4e13379918107b1
sholabioku <bilushonline@gmail.com>
2020-12-25 17:23:03 +0100

    Add deleteReview method with its functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -60,6 +60,9 @@ exports.addReview = asyncHandler(async (req, res, next) => {
   res.status(201).json({ success: true, data: review });
 });
 
+// @desc Update Review
+// @route PUT /api/v1/reviews/:id
+// @access Private
 exports.updateReview = asyncHandler(async (req, res, next) => {
   let review;
   review = await Review.findById(req.params.id);
@@ -81,4 +84,26 @@ exports.updateReview = asyncHandler(async (req, res, next) => {
   });
 
   res.status(200).json({ success: true, data: review });
+});
+
+// @desc Delete Review
+// @route DELETE /api/v1/reviews/:id
+// @access Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure review belong to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse('Not authorized to delete review', 401));
+  }
+
+  await Review.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({ success: true, data: {} });
 });
