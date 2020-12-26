cc01bdd4a520d98a15a35ebd76fe517aaf3efd35
sholabioku <bilushonline@gmail.com>
2020-12-26 07:01:35 +0100

    Add token from cookie headers

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,13 +11,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies) {
+    token = req.cookies.token;
   }
-  // else if (req.cookies) {
-  //   token = req.cookies.token;
-  // }
 
   if (!token) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
@@ -26,7 +25,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
