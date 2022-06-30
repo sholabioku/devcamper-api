ee806f075f81d1b456d17480a36a2faaf0c32980
sholabioku <bilushonline@gmail.com>
2022-06-30 17:58:15 +0100

    Add form inputs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Header';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <header>
-          <Navbar />
-        </header>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </Router>
   );
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Container, Form, Row, Col } from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <div className="showcase">
+      <div className="dark-overlay">
+        <div className="showcase-inner">
+          <Container>
+            <h1 className="display-4">Find a Code Bootcamp</h1>
+            <p className="lead">
+              Find, rate and read reviews on coding bootcamps
+            </p>
+            <Form action="bootcamps.html">
+              <Row>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="miles">
+                    <Form.Control
+                      type="text"
+                      name="miles"
+                      placeholder="Miles From"
+                      required
+                    />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="zipcode">
+                    <Form.Control
+                      type="text"
+                      name="zipcode"
+                      placeholder="Enter Zipcode"
+                      required
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Form.Control
+                type="submit"
+                value="Find Bootcamps"
+                className="btn btn-primary btn-block"
+              />
+            </Form>
+          </Container>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
