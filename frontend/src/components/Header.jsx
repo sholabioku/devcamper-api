1cb2e3f0aeab0ed934fda56c1329a6e13efce49d
sholabioku <bilushonline@gmail.com>
2022-07-02 15:02:05 +0100

    replaces tag elements with react-bootstrap markup

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -14,28 +14,18 @@ const Header = () => {
             </Navbar.Brand>
           </LinkContainer>
           <Nav className="me-auto w-100 justify-content-end">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  <i className="fas fa-sign-in-alt"></i> Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/register">
-                  <i className="fas fa-user-plus"></i> Register
-                </Link>
-              </li>
-              <li className="nav-item d-none d-sm-block">
-                <Link className="nav-link" to="#">
-                  |
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/bootcamps">
-                  Browse Bootcamps
-                </Link>
-              </li>
-            </ul>
+            <Link className="nav-link" to="/login">
+              <i className="fas fa-sign-in-alt"></i> Login
+            </Link>
+            <Link className="nav-link" to="/register">
+              <i className="fas fa-user-plus"></i> Register
+            </Link>
+            <Link className="nav-link" to="#">
+              |
+            </Link>
+            <Link className="nav-link" to="/bootcamps">
+              Browse Bootcamps
+            </Link>
           </Nav>
         </Container>
       </Navbar>
