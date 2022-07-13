7ff5e4f47e1883c82b775e8361fb1bc5a0ff9cb3
sholabioku <bilushonline@gmail.com>
2022-07-13 18:19:48 +0100

    Add loading

--- frontend/src/pages/bootcamps/Bootcamps.jsx
+++ frontend/src/pages/bootcamps/Bootcamps.jsx
@@ -145,38 +145,44 @@ const Bootcamps = () => {
           </Col>
           {/* Main Col */}
           <Col md={8}>
-            {bootcamps.map((bootcamp) => (
-              <Card className="mb-3" key={bootcamp._id}>
-                <Row className="no-gutters">
-                  <Col md={4}>
-                    <img
-                      src={bootcamp.image}
-                      className="card-img"
-                      alt={bootcamp.name}
-                    />
-                  </Col>
-                  <Col md={8}>
-                    <Card.Body>
-                      <Card.Title>
-                        <Link
-                          style={{ textDecoration: 'none' }}
-                          to={`/bootcamps/${bootcamp._id}`}
-                        >
-                          {bootcamp.name}
-                          <Badge bg="success" className="float-end">
-                            {bootcamp.averageRating}
-                          </Badge>
-                        </Link>
-                      </Card.Title>
-                      <Badge className="mb-2" bg="dark">
-                        {bootcamp.location.city}, {bootcamp.location.state}
-                      </Badge>
-                      <Card.Text>{bootcamp.careers}</Card.Text>
-                    </Card.Body>
-                  </Col>
-                </Row>
-              </Card>
-            ))}
+            {loading ? (
+              <div>Loading...</div>
+            ) : error ? (
+              <div>{error}</div>
+            ) : (
+              bootcamps.map((bootcamp) => (
+                <Card className="mb-3" key={bootcamp._id}>
+                  <Row className="no-gutters">
+                    <Col md={4}>
+                      <img
+                        src={bootcamp.image}
+                        className="card-img"
+                        alt={bootcamp.name}
+                      />
+                    </Col>
+                    <Col md={8}>
+                      <Card.Body>
+                        <Card.Title>
+                          <Link
+                            style={{ textDecoration: 'none' }}
+                            to={`/bootcamps/${bootcamp._id}`}
+                          >
+                            {bootcamp.name}
+                            <Badge bg="success" className="float-end">
+                              {bootcamp.averageRating}
+                            </Badge>
+                          </Link>
+                        </Card.Title>
+                        <Badge className="mb-2" bg="dark">
+                          {bootcamp.location.city}, {bootcamp.location.state}
+                        </Badge>
+                        <Card.Text>{bootcamp.careers}</Card.Text>
+                      </Card.Body>
+                    </Col>
+                  </Row>
+                </Card>
+              ))
+            )}
             <Pagination />
           </Col>
         </Row>
